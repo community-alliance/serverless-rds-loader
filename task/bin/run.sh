7d8fa49ca717b593b491881cf41eacd5dc61564d
larse514 <andrew.larsen@concordusa.com>
2018-05-09 23:21:19 -0500

    add image build and version

--- task/bin/run.sh
+++ task/bin/run.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e
+echo $DBURL
+echo $DBNAME
+echo $SECRETID
+echo $BUCKET
+echo $KEY
+username=`aws secretsmanager  get-secret-value --secret-id ${SECRETID} --query '{SecretString:SecretString}' | jq -r .SecretString | jq '.username'`
+password=`aws secretsmanager  get-secret-value --secret-id ${SECRETID} --query '{SecretString:SecretString}' | jq -r .SecretString | jq '.password'`
+
+aws s3 cp s3://$BUCKET/$KEY .
+
+mysql -h ${DBURL} -u ${username} -p${password} ${DBNAME} < $KEY
 
