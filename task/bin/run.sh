64e6b578f1bf620fe630efc2e94804e286f27287
larse514 <andrew.larsen@concordusa.com>
2018-05-21 20:41:57 -0500

    add log statements

--- task/bin/run.sh
+++ task/bin/run.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-echo $DBURL
-echo $DBNAME
-echo $SECRETID
-echo $BUCKET
-echo $KEY
+echo "DB URL $DBURL"
+echo "DB NAME $DBNAME"
+echo "SECRET ID $SECRETID"
+echo "BUCKET $BUCKET"
+echo "KEY $KEY"
+
 username=`aws secretsmanager  get-secret-value --secret-id ${SECRETID} --query '{SecretString:SecretString}' | jq -r .SecretString | jq '.username'`
 password=`aws secretsmanager  get-secret-value --secret-id ${SECRETID} --query '{SecretString:SecretString}' | jq -r .SecretString | jq '.password'`
 
 aws s3 cp s3://$BUCKET/$KEY .
-
+echo "username ${username}"
+echo "password ${password}"
 mysql -h ${DBURL} -u ${username} -p${password} ${DBNAME} < $KEY
 
